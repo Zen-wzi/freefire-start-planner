e16a075b3a07f541323d8fe6a8b6ec1b1d0d8453
aceaayush <aayushnikam0@gmail.com>
2025-12-31 19:41:30 +0530

    Initial commit without node_modules

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Free Fire Strategy Planner</h1>
+      <p>Frontend is working.</p>
+    </div>
+  );
+}
+
